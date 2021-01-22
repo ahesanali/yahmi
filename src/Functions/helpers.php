147e6104bc9727bb8a4668b7a7daf816205d76b3
Ahesanali Suthar <ahesanali.suthar@gmail.com>
2021-01-22 18:10:37 +0530

    Fixed: resourses path based on localhost and domain based development

--- src/Functions/helpers.php
+++ src/Functions/helpers.php
@@ -130,10 +130,14 @@ if (! function_exists('public_path')) {
  * Like css,js, or image path
  */
 if (! function_exists('asset')) {
-	function asset($asset_file_name)
-	{
-		return config('app.app_dir').config('app.assets_dir').DIRECTORY_SEPARATOR.$asset_file_name;
-	}
+    function asset($asset_file_name)
+    {
+        if(config('app.app_dir') == "/"){
+            return config('app.assets_dir').DIRECTORY_SEPARATOR.$asset_file_name;
+        }else{
+            return config('app.app_dir').config('app.assets_dir').DIRECTORY_SEPARATOR.$asset_file_name;
+        }        
+    }
 }
 
 /**
